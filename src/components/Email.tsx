efe0acf44ef57eea2919782a3a63369d12a96c4b
Egor Savenko <smahelzed@gmail.com>
2024-05-27 21:53:30 +0300

    initial commit

---
+++ src/components/Email.tsx
@@ -0,0 +1,7 @@
+import styles from './Email.module.css';
+
+const Email = () => {
+  return <div>App</div>
+};
+
+export default Email;
---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Email from './components/Email.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Email />
+  </React.StrictMode>,
+)
